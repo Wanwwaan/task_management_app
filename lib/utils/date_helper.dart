c30a809c91251c1dbd3300378edbda603aade835
Wanwwaan <thipnirun.08@gmail.com>
2024-02-16 20:14:41 +0700

    Update date format

--- lib/utils/date_helper.dart
+++ lib/utils/date_helper.dart
@@ -2,6 +2,6 @@ import 'package:moment_dart/moment_dart.dart';
 
 class DateHelper {
   static String dateGroupSeparator(String date) {
-    return Moment(DateTime.parse(date)).format('DD MMMM YYYY');
+    return Moment(DateTime.parse(date)).format('D MMMM YYYY');
   }
 }
