4eba072e9597d837cc62c332e7ea51e839cec9cc
Wanwwaan <thipnirun.08@gmail.com>
2024-02-17 16:09:50 +0700

    Fetch all tasks data

--- lib/screen/main/controllers/main_controller.dart
+++ lib/screen/main/controllers/main_controller.dart
@@ -18,7 +18,7 @@ class MainController extends GetxController {
   RxList<Task> doneList = <Task>[].obs;
   var isLoadmore = false.obs;
 
-  Future<void> fetchApi() async {
+  Future<void> fetchTodoData([bool fetchAllData = false]) async {
     int offsetNumber = 0;
     if (todoData != null) {
       if (isLoadmore.value) {
@@ -34,7 +34,7 @@ class MainController extends GetxController {
       }
     }
 
-    final result = await taskService.getTaskList(offsetNumber, 10, currentTab.value);
+    final result = await taskService.getTaskList(offsetNumber, 10, 'TODO');
     if (result != null) {
       if (todoData != null) {
         if (result.tasks.isNotEmpty) {
@@ -52,14 +52,113 @@ class MainController extends GetxController {
     }
   }
 
-  Future<void> onTabTask(String key) async {
-    currentTab.value = key;
-    await fetchApi();
+  Future<void> fetchDoingData([bool fetchAllData = false]) async {
+    int offsetNumber = 0;
+    if (doingData != null) {
+      if (isLoadmore.value) {
+        int pageNumber = doingData!.pageNumber;
+        int totalPages = doingData!.totalPages;
+        if (pageNumber < totalPages) {
+          offsetNumber = pageNumber + 1;
+        } else {
+          offsetNumber = pageNumber;
+        }
+      } else {
+        offsetNumber = doingData!.pageNumber;
+      }
+    }
+
+    final result = await taskService.getTaskList(offsetNumber, 10, 'DOING');
+    if (result != null) {
+      if (doingData != null) {
+        if (result.tasks.isNotEmpty) {
+          doingData = result;
+          if (doingList.isNotEmpty) {
+            doingList.addAll(result.tasks);
+          }
+        }
+      } else {
+        doingData = result;
+        doingList(result.tasks);
+      }
+    } else {
+      //TODO:Display error alert
+    }
+  }
+
+  Future<void> fetchDoneData([bool fetchAllData = false]) async {
+    int offsetNumber = 0;
+    if (doneData != null) {
+      if (isLoadmore.value) {
+        int pageNumber = doneData!.pageNumber;
+        int totalPages = doneData!.totalPages;
+        if (pageNumber < totalPages) {
+          offsetNumber = pageNumber + 1;
+        } else {
+          offsetNumber = pageNumber;
+        }
+      } else {
+        offsetNumber = doneData!.pageNumber;
+      }
+    }
+
+    final result = await taskService.getTaskList(offsetNumber, 10, 'DONE');
+    if (result != null) {
+      if (doneData != null) {
+        if (result.tasks.isNotEmpty) {
+          doneData = result;
+          if (doneList.isNotEmpty) {
+            doneList.addAll(result.tasks);
+          }
+        }
+      } else {
+        doneData = result;
+        doneList(result.tasks);
+      }
+    } else {
+      //TODO:Display error alert
+    }
+  }
+
+  Future<void> fetchData(String currentTab) async {
+    switch (currentTab) {
+      case 'TODO':
+        await fetchTodoData();
+        break;
+      case 'DOING':
+        await fetchDoingData();
+        break;
+      case 'DONE':
+        await fetchDoneData();
+      default:
+    }
+  }
+
+  Future<void> onTabTask([String? tab = '']) async {
+    var tabKey = '';
+    if (tab == '') {
+      tabKey = currentTab.value;
+    } else {
+      tabKey = tab!;
+    }
+    currentTab.value = tabKey;
+
+    switch (tabKey) {
+      case 'TODO':
+        await fetchTodoData();
+        break;
+      case 'DOING':
+        await fetchDoingData();
+        break;
+      case 'DONE':
+        await fetchDoneData();
+      default:
+    }
   }
 
   Future<void> loadMore() async {
     isLoadmore.value = true;
-    await fetchApi();
+    await onTabTask();
     isLoadmore.value = false;
   }
 
@@ -69,10 +168,16 @@ class MainController extends GetxController {
     }
   }
 
+  Future<void> fetchAllTasks() async {
+    await fetchTodoData();
+    await fetchDoingData();
+    await fetchDoneData();
+  }
+
   @override
   void onInit() async {
     super.onInit();
-    await fetchApi();
+    await fetchAllTasks();
     scrollController.addListener(scrollerListener);
   }
 
